6d70dba6bbb467db03bc29471338e4d03f11453e
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-09-03 18:28:10 +0200

    Fullført oppgave 1

---
+++ 3-Intro-Javascript/Oppgave_1/oppgave1.js
@@ -0,0 +1,14 @@
+const text =
+  "Baby cliche unicorn brooklyn farm-to-table. Salvia semiotics hella literally paleo humblebrag bushwick letterpress messenger bag pork belly brooklyn authentic vexillologist. Gastropub sustainable banjo, shaman snackwave viral air plant ramps health goth. Edison bulb vegan microdosing, tote bag unicorn skateboard disrupt copper mug four loko sustainable whatever cloud bread slow-carb lumbersexual four dollar toast. Waistcoat lomo hammock vape shabby chic sartorial yr godard pok pok fashion axe organic migas. Quinoa yr vexillologist intelligentsia verylongwordthatislong neutra mixtape YOLO XOXO listicle letterpress farm-to-table beard.";
+
+const longestWord = () => {
+  const wordList = text.split(" ");
+  let longestWord = "";
+  wordList.forEach((word) => {
+    longestWord = longestWord.length < word.length ? word : longestWord;
+  });
+
+  return longestWord;
+};
+
+console.log(longestWord());
