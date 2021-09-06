5f5f538c33a3131834272f7cdd2f6d33907ad6af
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-09-06 15:18:34 +0200

    Refaktorert funksjon til å bruke reduce

--- 3-Intro-Javascript/Oppgave_1/oppgave1.js
+++ 3-Intro-Javascript/Oppgave_1/oppgave1.js
@@ -1,14 +1,11 @@
 const text =
   "Baby cliche unicorn brooklyn farm-to-table. Salvia semiotics hella literally paleo humblebrag bushwick letterpress messenger bag pork belly brooklyn authentic vexillologist. Gastropub sustainable banjo, shaman snackwave viral air plant ramps health goth. Edison bulb vegan microdosing, tote bag unicorn skateboard disrupt copper mug four loko sustainable whatever cloud bread slow-carb lumbersexual four dollar toast. Waistcoat lomo hammock vape shabby chic sartorial yr godard pok pok fashion axe organic migas. Quinoa yr vexillologist intelligentsia verylongwordthatislong neutra mixtape YOLO XOXO listicle letterpress farm-to-table beard.";
 
-const longestWord = () => {
-  const wordList = text.split(" ");
-  let longestWord = "";
-  wordList.forEach((word) => {
-    longestWord = longestWord.length < word.length ? word : longestWord;
-  });
+const longestWord = () =>
+  text
+    .split(" ")
+    .reduce((max, word) => (word.length > max.length ? word : max));
 
-  return longestWord;
-};
-
-console.log(longestWord());
+document.getElementById(
+  "app"
+).innerHTML = `Det lengste ordet i strengen er: ${longestWord().bold()}`;
